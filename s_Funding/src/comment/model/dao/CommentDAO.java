40637a0bf618cfec4b8feaecba6b8321ebd528b9
lovesykim0115 <ang11585@naver.com>
2020-05-24 22:00:00 +0900

    Create CommentDAO.java
    commentDAO

---
+++ s_Funding/src/comment/model/dao/CommentDAO.java
@@ -0,0 +1,42 @@
+package comment.model.dao;
+
+import java.util.ArrayList;
+
+import comment.service.Comment;
+
+public class CommentDAO {
+	//댓글 생성
+		public int insertComment() {
+			return 0;
+		}
+		
+		//댓글 수정
+		public int modifyComment() {
+			return 0;
+		}
+		
+		// 댓글 삭제
+		public int deleteComment() {
+			return 0;
+		}
+		
+		// 댓글 검색
+		public int searchComment() {
+			return 0;
+		}
+		
+		// 댓글 한개 보여줌
+		public int selectComment() {
+			return 0;
+		}
+		
+		// 댓글 리스트로 가져와서 보여줌
+		public ArrayList<Comment> selectCommentList() {
+			
+		}
+		
+		// 댓글 갯수 새기 (필요하면 쓰기)
+		public int getCommentCount() {
+			return 0;
+		}
+}
