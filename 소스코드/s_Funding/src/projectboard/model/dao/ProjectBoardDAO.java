fc292adbcd020af6b3cb473485aeb21f69bdbeee
seohuiJo <41426853+seohuiJo@users.noreply.github.com>
2020-05-25 16:41:31 +0900

    commit 시연s

---
+++ 소스코드/s_Funding/src/projectboard/model/dao/ProjectBoardDAO.java
@@ -0,0 +1,40 @@
+package projectboard.model.dao;
+
+import java.util.ArrayList;
+
+import projectboard.model.vo.ProjectBoard;
+
+public class ProjectBoardDAO {
+	
+	//프로젝트 게시글 작성
+	public int writeProject() {
+		return 0;
+	}
+	
+	// 프로젝트 삭제
+	public int deleteProject() {
+		return 0;
+	}
+	
+	//프로젝트 검색
+	public int searchProject() {
+		return 0;
+	}
+	
+	// 프로젝트 select
+	public int selectProject() {
+		return 0;
+	}
+	
+	// 추천 프로젝트 list로 가져와서 보기
+	public ArrayList<ProjectBoard> recommendProject() {
+		return null;
+	
+	}
+	
+	//page숫자 메기기
+	public ArrayList<ProjectBoard> selelctProjectList() {
+		return null;
+	}
+
+}
