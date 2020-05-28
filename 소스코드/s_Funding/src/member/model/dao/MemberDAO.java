be44bca7e9225a058c31fde447ef5e690362fa76
lovesy_kim <ang11585@naver.com>
2020-05-28 19:17:11 +0900

    회원전체조회
    commit

--- 소스코드/s_Funding/src/member/model/dao/MemberDAO.java
+++ 소스코드/s_Funding/src/member/model/dao/MemberDAO.java
@@ -10,30 +10,41 @@ import java.util.ArrayList;
 import member.model.vo.Member;
 
 public class MemberDAO {
-	
+
 	// 전체 멤버 가져오기
 	public ArrayList<Member> selectMemberList(Connection conn) {
-		  ArrayList<Member> list = null;
-	      Statement stmt = null;
-	      ResultSet rset = null;
-	      String query = "select * from member";
-	      
-	      try {
-	         stmt=conn.createStatement();
-	         rset = stmt.executeQuery(query);
-	         list = new ArrayList<Member>();
-	          while(rset.next()) {
-	                  Member member = new Member();
-	               member.setUserId(rset.getString("user_id"));
-	               list.add(member);
-	          }
+		ArrayList<Member> list = null;
+		Statement stmt = null;
+		ResultSet rset = null;
+		String query = "select * from member";
 
-	      } catch (SQLException e) {
-	         // TODO Auto-generated catch block
-	         e.printStackTrace();
-	      }
-	      
-	      return list;
+		try {
+			stmt = conn.createStatement();
+			rset = stmt.executeQuery(query);
+			list = new ArrayList<Member>();
+			while (rset.next()) {
+				Member member = new Member();
+				member.setUserId(rset.getString("user_id"));
+				member.setUserPwd(rset.getString("user_pwd"));
+				member.setUserName(rset.getString("user_name"));
+				member.setPhone(rset.getString("phone"));
+				member.setNickname(rset.getString("nickname"));
+				member.setAddress(rset.getString("address"));
+				member.setEmail(rset.getString("email"));
+				member.setPoint(rset.getInt("point"));
+				member.setEnabled(rset.getInt("enabled"));
+				member.setuRegdate(rset.getDate("u_regdate"));
+				member.setInterest(rset.getString("interest"));
+
+				list.add(member);
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return list;
 	}
 
 }
